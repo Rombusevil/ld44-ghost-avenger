9ab2dfe0cc51af16b0c49c51953c8bc0e9d665f9
Iber <iberparodisiri@gmail.com>
2019-04-28 20:18:51 -0300

    Instructions

--- instructions_state.lua
+++ instructions_state.lua
@@ -2,27 +2,25 @@
 function instructions_state()
     local s={}
     local texts={}
-    local timeout=2 -- for avoiding the user hitting X while playing and by that dismissing this screen. In seconds
-
-    -- graphical frame 
+	
+	-- graphical frame 
     local frbkg=1
     local frfg=6
     
     local ty=15
     add(texts, tutils({text="instructions",centerx=true,y=ty,fg=8,bg=0,bordered=true,shadowed=true,sh=2})) ty+=10
-    add(texts, tutils({text="-protect the ghost       " ,centerx=true,y=ty,fg=8,bg=0,bordered=true,shadowed=true,sh=2}))ty+=10
-    add(texts, tutils({text="-destroy all the spawners",centerx=true,y=ty,fg=8,bg=0,bordered=true,shadowed=true,sh=2})) ty+=10
-    add(texts, tutils({text="-o button takes life from",centerx=true,y=ty,fg=8,bg=0,bordered=true,shadowed=true,sh=2})) ty+=10
-    add(texts, tutils({text="the ghost. Use it wisely  ",centerx=true,y=ty,fg=8,bg=0,bordered=true,shadowed=true,sh=2}))ty+=10
+    add(texts, tutils({text="-protect the ghost       ",centerx=true,y=ty,fg=0,bg=0,bordered=false,shadowed=false,sh=2}))ty+=10
+    add(texts, tutils({text="-destroy all the spawners",centerx=true,y=ty,fg=0,bg=0,bordered=false,shadowed=false,sh=2})) ty+=10
+    add(texts, tutils({text="-🅾️ button takes life from",centerx=true,y=ty,fg=0,bg=0,bordered=false,shadowed=false,sh=2})) ty+=10
+    add(texts, tutils({text=" the ghost. use it wisely",centerx=true,y=ty,fg=0,bg=0,bordered=false,shadowed=false,sh=2}))ty+=10
     
-    local restart_msg = "press ❎ to restart"
+    local cmsg = "press ❎ to continue"
     local msg = tutils({text="", blink=true, on_time=15, centerx=true,y=110,fg=0,bg=1,bordered=false,shadowed=true,sh=7})
     add(texts, msg)
 
     s.update=function()
         camera(0,0)
-        timeout -= 1/60
-        if(btnp(5) and timeout <= 0) curstate=menu_state() -- "X"
+        if(btnp(5)) curstate=game_state() -- "X"
     end
 
     cls()
@@ -46,12 +44,7 @@ function instructions_state()
         rectfill(frame_x0+1,frame_x0+2, frame_x1-1, frame_y1-3, frfg)
                 
         -- draw texts
-        if(timeout > 0)then
-            local t = flr(timeout) + 1
-            msg.text = "wait for it... ("..t..")"
-        else
-            msg.text = restart_msg
-        end
+		msg.text = cmsg
 
         for t in all(texts) do
             t:draw()
